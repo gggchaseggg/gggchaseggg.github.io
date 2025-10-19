579e3a31109795cae3fb59f897daca3892da2f29
Daniil Grachev <gggchaseggg@gmail.com>
2025-10-19 13:34:02 +0300

    set up application

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import path from 'path'
+
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/gggchaseggg.github.io'
+  resolve: {
+    alias: {
+      '@tt': path.resolve(__dirname, './src'),
+    },
+  },
 })
